b271f8f7df9d92c3e1e2d379cfeb5027cd8f0b7b
Karataev Daniil <dirings52445@gmail.com>
2026-01-07 10:50:49 +0200

    fix

--- src/pages/Catalog/CatalogPage.jsx
+++ src/pages/Catalog/CatalogPage.jsx
@@ -1,36 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductCard from "../../features/product/ProductCard";
 import products from "../../data/products";
+import ProductGrid from "../../features/catalog/ProductGrid";
+import ProductFilters from "../../features/catalog/ProductFilters";
 import styles from "./CatalogPage.module.css";
 
 function CatalogPage() {
 	const { category } = useParams();
 	
-	const filteredProducts = category
+	const productsByCategory = category
 	  ? products.filter(p => p.categories.includes(category))
 	  : products;
 
 
+	const [categoryFilter, setCategoryFilter] = useState("all");
+    const [tagsFilter, setTagsFilter] = useState("all");
+
+    const filteredProducts = products.filter(p => {
+        const categoryMatch = categoryFilter === "all" || p.categories.includes(categoryFilter);
+        const tagMatch = tagsFilter === "all" || p.tags.includes(tagsFilter);
+        return categoryMatch && tagMatch;
+    });
+
+    const categories = [...new Set(products.flatMap(p => p.categories))];
+    const tags = [...new Set(products.flatMap(p => p.tags))];
 	return (
 		<main className={styles.catalog}>
+			<section className={styles.products}>
+				<h1>
+					{category ? `Категория: ${category}` : "Каталог мебели"}
+				</h1>
 
-			<h1>
-				{category ? `Категория: ${category}` : "Каталог мебели"}
-			</h1>
+				<ProductGrid 
+					products={productsByCategory} 
+					itemsPerPage={36}
+				/>
+			</section>
 
-			<div className={styles.productsGrid}>
-				{filteredProducts.map(product => (
-					<ProductCard
-						key={product.id}
-						id={product.id}
-						image={product.image}
-						name={product.name}
-						price={product.price}
-					/>
-				))}
-			</div>
-
+			<section className={styles.products}>
+                <h2>Поиск по другим категориям</h2>
+                <ProductFilters
+                    categories={categories}
+                    tags={tags}
+                    categoryFilter={categoryFilter}
+                    setCategoryFilter={setCategoryFilter}
+                    tagsFilter={tagsFilter}
+                    setTagsFilter={setTagsFilter}
+                />
+                {filteredProducts.length === 0 ? (
+                    <div>Ничего не найдено!</div>
+                ) : (
+                    <ProductGrid 
+                        products={filteredProducts} 
+                        mode="loadMore"
+                    />
+                )}
+            </section>
 		</main>
 	);
 }
